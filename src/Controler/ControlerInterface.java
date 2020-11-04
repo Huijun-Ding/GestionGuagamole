1e8ae181a6e7ce99dc7fe1c53da55410a5b8b549
chengyuduan <duanchengyu995@gmail.com>
2020-11-04 13:30:38 +0100

    Update ControlerInterface.java

--- src/Controler/ControlerInterface.java
+++ src/Controler/ControlerInterface.java
@@ -1,5 +1,21 @@
 package Controler;
 
+import Model.*;
+import Vue.*;
+
 public class ControlerInterface {
-    
+	private Model model;
+	
+	public ControlerInterface() {
+		this.model = new Model();
+	}
+	
+    public AdminMateriel connexion(String id, String mdp, int i) {
+    	
+    	if(model.connexion(id, mdp, i)!=null) {   		
+    		AdminMateriel am = model.connexion(id, mdp, i);
+    		return am;
+    	}
+    	return null;
+    }
 }

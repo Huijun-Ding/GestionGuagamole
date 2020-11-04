bc6ec34ecdb8ba9e9cfa09f73593ca93031addaf
chengyuduan <duanchengyu995@gmail.com>
2020-11-04 11:31:52 +0100

    Update GestionGuagamole.java

--- src/gestionguagamole/GestionGuagamole.java
+++ src/gestionguagamole/GestionGuagamole.java
@@ -1,9 +1,15 @@
 package gestionguagamole;
 
+import Controler.ControlerInterface;
+import Model.Model;
+import Vue.Connexion;
+
 public class GestionGuagamole {
 
     public static void main(String[] args) {
-        
+        ControlerInterface controler = new ControlerInterface();
+        Model model = new Model();
+        Connexion c1 = new Connexion(controler);
     }
     
 }

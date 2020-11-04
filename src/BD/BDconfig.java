6117013377a9994583deda7a6bbc4019455bc24e
chengyuduan <duanchengyu995@gmail.com>
2020-11-04 18:28:30 +0100

    Update BDconfig.java

--- src/BD/BDconfig.java
+++ src/BD/BDconfig.java
@@ -1,5 +1,23 @@
 package BD;
-
+import java.sql.*;
 public class BDconfig {
-    
+    Connection con;
+    public Connection getConnection() {
+    	try {
+    		Class.forName("com.mysql.jdbc.Driver");
+    	}catch(ClassNotFoundException e) {
+    		e.printStackTrace();
+    	}
+    	try {
+    		con=DriverManager.getConnection("jdbc:mysql:"+"//127.0.0.1:3306/gestionguagamole","root","");
+    		System.out.println("success");
+    	}catch(SQLException e) {
+    		e.printStackTrace();
+    	}
+    	return con;
+    }
+    public static void main(String[] args) {
+    	BDconfig c = new BDconfig();
+    	c.getConnection();
+    }
 }

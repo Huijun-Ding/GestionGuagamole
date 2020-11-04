433446bfd45f141c2f52f18db05ba04d28db3f2c
chengyuduan <duanchengyu995@gmail.com>
2020-11-04 20:33:45 +0100

    Update Etudiant.java

--- src/Model/Etudiant.java
+++ src/Model/Etudiant.java
@@ -4,7 +4,7 @@ public class Etudiant extends Utilisateur{
     
     private Groupe groupe;
     
-    public Etudiant(String num, String mdp, String nom, String prenom, Groupe p) {
+    public Etudiant(int num, String mdp, String nom, String prenom, Groupe p) {
         super(num, mdp, nom, prenom);
         this.groupe = p;
     }

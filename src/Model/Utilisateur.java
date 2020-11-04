47a9c19d024c7d81e29820e6afe4e2f2393749df
chengyuduan <duanchengyu995@gmail.com>
2020-11-04 20:33:49 +0100

    Update Utilisateur.java

--- src/Model/Utilisateur.java
+++ src/Model/Utilisateur.java
@@ -3,13 +3,13 @@ package Model;
 import java.util.ArrayList;
 
 public class Utilisateur {
-    private String numU;
+    private int numU;
     private String mdpU;
     private String nomU;
     private String prenomU;
     private ArrayList<Reservation> mesReservation;
     
-    public Utilisateur(String num, String mdp, String nom, String prenom) {
+    public Utilisateur(int num, String mdp, String nom, String prenom) {
         this.numU = num;
         this.mdpU = mdp;
         this.nomU = nom;

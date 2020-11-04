f0197863e22b71d3096d275e603ee0ea8817fe76
chengyuduan <duanchengyu995@gmail.com>
2020-11-04 11:32:02 +0100

    Update AjouterMachine.java

--- src/Vue/AjouterMachine.java
+++ src/Vue/AjouterMachine.java
@@ -2,36 +2,23 @@ package Vue;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
-public class AjouterMachine extends JFrame {
+public class AjouterMachine extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
-	private JTextField txtNumMAM;
-	private JTextField txtNumSAM;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AjouterMachine frame = new AjouterMachine();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
+	private static JButton btnValiderAM = new JButton("Valider");
+	private static JButton btnAnnulerAM = new JButton("Annuler");
 	/**
 	 * Create the frame.
 	 */
@@ -44,34 +31,50 @@ public class AjouterMachine extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Ajouter une machine");
-		lblNewLabel.setBounds(133, 6, 142, 16);
+		lblNewLabel.setBounds(154, 25, 142, 16);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Numéro de machine:");
+		JLabel lblNewLabel_1 = new JLabel("Numero de machine:");
 		lblNewLabel_1.setBounds(36, 81, 142, 16);
 		contentPane.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Numéro de salle:");
+		JLabel lblNewLabel_2 = new JLabel("Numero de salle:");
 		lblNewLabel_2.setBounds(40, 144, 142, 16);
 		contentPane.add(lblNewLabel_2);
 		
-		txtNumMAM = new JTextField();
+		JTextField txtNumMAM = new JTextField();
 		txtNumMAM.setBounds(238, 80, 130, 26);
 		contentPane.add(txtNumMAM);
 		txtNumMAM.setColumns(10);
 		
-		txtNumSAM = new JTextField();
+		JTextField txtNumSAM = new JTextField();
 		txtNumSAM.setBounds(239, 139, 130, 26);
 		contentPane.add(txtNumSAM);
 		txtNumSAM.setColumns(10);
 		
-		JButton btnValiderAM = new JButton("Valider");
+		
 		btnValiderAM.setBounds(260, 215, 117, 29);
 		contentPane.add(btnValiderAM);
 		
-		JButton btnAnnulerAM = new JButton("Annuler");
+		
 		btnAnnulerAM.setBounds(75, 216, 117, 29);
 		contentPane.add(btnAnnulerAM);
+		
+		btnValiderAM.addActionListener(this);
+		btnAnnulerAM.addActionListener(this);
+		
+		this.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnValiderAM) {
+			this.dispose();
+			GestionMachine gm = new GestionMachine();
+		}if(e.getSource()==btnAnnulerAM) {
+			this.dispose();
+			GestionMachine gm1 = new GestionMachine();
+		}
+		
 	}
 
 }

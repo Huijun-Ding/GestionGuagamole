6e7c0c88e88b475e6ebce87e98d24f421d3b058c
chengyuduan <duanchengyu995@gmail.com>
2020-11-04 14:23:26 +0100

    Create HomePageAdminM.java

---
+++ src/Vue/HomePageAdminM.java
@@ -0,0 +1,88 @@
+package Vue;
+
+import java.awt.BorderLayout;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import Controler.ControlerInterface;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class HomePageAdminM extends JFrame implements ActionListener{
+
+	private JPanel contentPane;
+	private ControlerInterface controler;
+	/**
+	 * Launch the application.
+	 */
+
+	private static JButton btnSeDeconnecter = new JButton("Se Deconnecter");
+	private static JButton btnGestionSalle = new JButton("Gestion des salles");
+	private static JButton btnGestionMachine = new JButton("Gestion des machines");
+	/**
+	 * Create the frame.
+	 */
+	public HomePageAdminM() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Bienvenue!");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		lblNewLabel.setBounds(23, 6, 103, 36);
+		contentPane.add(lblNewLabel);
+		
+		JLabel nomAdminM = new JLabel("DUAN");
+		nomAdminM.setBounds(205, 17, 89, 16);
+		contentPane.add(nomAdminM);
+		
+		JLabel prenomAdminM = new JLabel("Chengyu");
+		prenomAdminM.setBounds(119, 17, 83, 16);
+		contentPane.add(prenomAdminM);
+		
+		
+		
+		btnSeDeconnecter.setBounds(304, 24, 124, 29);
+		contentPane.add(btnSeDeconnecter);
+		
+		
+		btnGestionSalle.setBounds(40, 136, 168, 52);
+		contentPane.add(btnGestionSalle);
+		
+		
+		btnGestionMachine.setBounds(259, 136, 168, 52);
+		contentPane.add(btnGestionMachine);
+		
+		btnSeDeconnecter.addActionListener(this);
+		btnGestionSalle.addActionListener(this);
+		btnGestionMachine.addActionListener(this);
+		
+		this.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnSeDeconnecter) {
+			this.dispose();
+			Connexion con = new Connexion(controler);
+		}if(e.getSource()==btnGestionSalle) {
+			this.dispose();
+			GestionSalle gs = new GestionSalle();
+		}if(e.getSource()==btnGestionMachine) {
+			this.dispose();
+			GestionMachine gm = new GestionMachine();
+		}
+		
+	}
+
+}

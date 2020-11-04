ef8b6eb2c40badfbb515cf79ea09318f5a45725a
chengyuduan <duanchengyu995@gmail.com>
2020-11-04 23:32:38 +0100

    Update Salle.java

--- src/Model/Salle.java
+++ src/Model/Salle.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Salle {
-    private String numSalle;
+    private int numSalle;
+    private String nom;
     private int capaciteSalle;
     private HashMap<Calendrier,Boolean> etatSalle = new HashMap<Calendrier,Boolean>();
     private ArrayList<Machine> machines;
     
-    public Salle(String num, int nb) {
+    public Salle(int num, String nom, int nb) {
         this.numSalle = num;
+        this.nom=nom;
         this.capaciteSalle = nb;
         machines = new ArrayList();
     }
     
+    public String getNomSalle() {
+    	return this.nom;
+    }
     public void setEtatSalle(Calendrier c, boolean etat) {
 
     }

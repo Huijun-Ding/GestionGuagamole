8870796d0011f25dd0febd889f5abd57ab109c62
Miangola <rakotoarisoamiangola@gmail.com>
2020-11-05 00:17:43 +0100

    Update Utilisateur.java

--- src/Model/Utilisateur.java
+++ src/Model/Utilisateur.java
@@ -1,27 +1,20 @@
 package Model;
 
-import java.util.ArrayList;
+
 
 public class Utilisateur {
-    private String numU;
-    private String mdpU;
-    private String nomU;
-    private String prenomU;
-    protected ArrayList<Reservation> mesReservation;
+    protected String numU;
+    protected String mdpU;
+    protected String nomU;
+    protected String prenomU;
     
     public Utilisateur(String num, String mdp, String nom, String prenom) {
         this.numU = num;
         this.mdpU = mdp;
         this.nomU = nom;
         this.prenomU = prenom;
-        this.mesReservation = new ArrayList<>();
     }
 
-	public ArrayList<Reservation> getMesReservation() {
-		return mesReservation;
-	}
+	
 
-	public void setMesReservation(ArrayList<Reservation> mesReservation) {
-		this.mesReservation = mesReservation;
-	}
 }

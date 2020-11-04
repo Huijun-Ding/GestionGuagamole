8fa8758ec474ac9db56924443b26b7b7108b8f45
chengyuduan <duanchengyu995@gmail.com>
2020-11-04 20:33:38 +0100

    Update ControlerInterface.java

--- src/Controler/ControlerInterface.java
+++ src/Controler/ControlerInterface.java
@@ -2,19 +2,26 @@ package Controler;
 
 import Model.*;
 import Vue.*;
+import BD.*;
+
 
 public class ControlerInterface {
-	private Model model;
-	
+	private Query query;
 	public ControlerInterface() {
-		this.model = new Model();
+		this.query = new Query();
 	}
 	
-    public AdminMateriel connexion(String id, String mdp, int i) {
-    	
-    	if(model.connexion(id, mdp, i)!=null) {   		
-    		AdminMateriel am = model.connexion(id, mdp, i);
-    		return am;
+    public Utilisateur connexion(int id, String mdp, int i) {
+    	Utilisateur user = query.ConnexionUilisateur(id, mdp, i);
+    	if(user!=null) {
+    		if(user instanceof AdminMateriel) {
+    			AdminMateriel am = (AdminMateriel) query.ConnexionUilisateur(id, mdp, i);
+        		return am;
+    		}
+    		if(user instanceof AdminRespoF) {
+    			AdminRespoF ar = (AdminRespoF) query.ConnexionUilisateur(id, mdp, i);
+    			return ar;
+    		}
     	}
     	return null;
     }

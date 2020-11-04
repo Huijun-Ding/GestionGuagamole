563461b26f1461810888ceb96b6862ad241e7f6f
chengyuduan <duanchengyu995@gmail.com>
2020-11-04 11:32:06 +0100

    Update AjouterSalle.java

--- src/Vue/AjouterSalle.java
+++ src/Vue/AjouterSalle.java
@@ -2,36 +2,25 @@ package Vue;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
-public class AjouterSalle extends JFrame {
+public class AjouterSalle extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
-	private JTextField txtNumS;
-	private JTextField txtCS;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AjouterSalle frame = new AjouterSalle();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	private static JButton btnValiderAjouterSalle = new JButton("Valider");
+	private static JButton btnAnnulerAjouterSalle = new JButton("Annuler");
 	/**
 	 * Create the frame.
 	 */
@@ -47,30 +36,46 @@ public class AjouterSalle extends JFrame {
 		lblNewLabel.setBounds(155, 6, 123, 16);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Numéro de salle:");
+		JLabel lblNewLabel_1 = new JLabel("Numero de salle:");
 		lblNewLabel_1.setBounds(95, 80, 114, 16);
 		contentPane.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Capacité:");
+		JLabel lblNewLabel_2 = new JLabel("Capacite:");
 		lblNewLabel_2.setBounds(95, 162, 114, 16);
 		contentPane.add(lblNewLabel_2);
 		
-		txtNumS = new JTextField();
+		JTextField txtNumS = new JTextField();
 		txtNumS.setBounds(244, 75, 130, 26);
 		contentPane.add(txtNumS);
 		txtNumS.setColumns(10);
 		
-		txtCS = new JTextField();
+		JTextField txtCS = new JTextField();
 		txtCS.setBounds(244, 157, 130, 26);
 		contentPane.add(txtCS);
 		txtCS.setColumns(10);
 		
-		JButton btnValiderAjouterSalle = new JButton("Valider");
+		
 		btnValiderAjouterSalle.setBounds(257, 229, 117, 29);
 		contentPane.add(btnValiderAjouterSalle);
-		
-		JButton btnAnnulerAjouterSalle = new JButton("Annuler");
+				
 		btnAnnulerAjouterSalle.setBounds(92, 229, 117, 29);
 		contentPane.add(btnAnnulerAjouterSalle);
+		
+		btnValiderAjouterSalle.addActionListener(this);
+		btnAnnulerAjouterSalle.addActionListener(this);
+		
+		this.setVisible(true);
 	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnValiderAjouterSalle) {
+			this.dispose();
+			GestionSalle gs = new GestionSalle();
+		}if(e.getSource()==btnAnnulerAjouterSalle) {
+			this.dispose();
+			GestionSalle gs = new GestionSalle();
+		}
+		
+	}
+
 }

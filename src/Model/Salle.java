45f96980cc19757d10349670e89bb33ff66fa100
Huijun-Ding <mathilda.cnfr@gmail.com>
2020-11-05 10:09:32 +0100

    nomMachine

--- src/Model/Salle.java
+++ src/Model/Salle.java
@@ -6,20 +6,20 @@ import java.util.HashMap;
 
 public class Salle {
 
-    private String numSalle;
+    private String nomSalle;
     private int capaciteSalle;
     private HashMap<Calendrier, Boolean> etatSalle;
     private ArrayList<Machine> machines;
 
     public Salle(String num, int nb) {
-        this.numSalle = num;
+        this.nomSalle = num;
         this.capaciteSalle = nb;
         this.etatSalle = new HashMap<Calendrier, Boolean>();
         this.machines = new ArrayList<>();
     }
 
     public String getNumSalle() {
-        return numSalle;
+        return nomSalle;
     }
 
     public void setEtatSalle(Calendrier c, boolean etat) {

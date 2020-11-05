c2152dbf1dfb3383613d74c7527e865ea8eb2c1f
Huijun-Ding <mathilda.cnfr@gmail.com>
2020-11-05 10:07:01 +0100

    nomMachine

--- src/Model/Machine.java
+++ src/Model/Machine.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Machine {
-    private String numMachine;
+    private String nomMachine;
     private Salle salle;
     private ArrayList<Reservation> reservations;
     private HashMap<Calendrier,Boolean> etatMachine ;
     
     public Machine(String num, Salle s) {
-        this.numMachine = num;
+        this.nomMachine = num;
         this.salle = s;
         this.etatMachine= new HashMap<Calendrier,Boolean>();
         this.reservations = new ArrayList<>();
     }
     
     public String getNumMachine() {
-    	return numMachine;
+    	return nomMachine;
     }
 	
 }

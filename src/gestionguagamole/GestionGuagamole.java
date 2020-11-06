f15a539949a0e1baaf8eb87afb6d4ed0621b09d7
Miangola <rakotoarisoamiangola@gmail.com>
2020-11-06 09:46:01 +0100

    Update GestionGuagamole.java

--- src/gestionguagamole/GestionGuagamole.java
+++ src/gestionguagamole/GestionGuagamole.java
@@ -1,16 +1,22 @@
 package gestionguagamole;
 
+import java.sql.SQLException;
+
 import BD.*;
 import Controler.ControlerInterface;
 import Vue.Connexion;
 
 public class GestionGuagamole {
 
-    public static void main(String[] args) {
-        ControlerInterface controler = new ControlerInterface();
+    public static void main(String[] args) throws SQLException {
+    	ControlerInterface controler = new ControlerInterface();
         Query query = new Query();
         Connexion c1 = new Connexion(controler);
-        new BD.Query().getConnection();
+        BDconfig.getConnection();
+        
+      for (int i = 0; i < query.afficherNomTP("1","2020-11-12").size(); i++) {
+            System.out.println(query.afficherNomTP("1","2020-11-12").get(i));
+        }
     }
     
 }

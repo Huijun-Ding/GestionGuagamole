fb0bc39a04138c45d1daf4b4df246b3368504a1f
chengyuduan <duanchengyu995@gmail.com>
2020-11-06 11:08:10 +0100

    Update Connexion.java

--- src/Vue/Connexion.java
+++ src/Vue/Connexion.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import Controler.ControlerInterface;
+import Model.Utilisateur;
 import BD.Query;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -21,6 +22,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class Connexion extends JFrame implements ItemListener, ActionListener{
 
@@ -32,69 +34,89 @@ public class Connexion extends JFrame implements ItemListener, ActionListener{
 	private static JRadioButton rdEtudiant = new JRadioButton("Etudiant",false);
 	private static JRadioButton rdEnseignant = new JRadioButton("Enseignant",false);
 	private static JRadioButton rdRespoF = new JRadioButton("Respo. Formation",false);
+	private static JRadioButton rdAdminM = new JRadioButton("Admin Materiel",false);
 	private static JLabel lblNewLabel = new JLabel("Connexion");
 	private static JLabel lblNewLabel_1 = new JLabel("Identifiant:");
 	private static JLabel lblNewLabel_2 = new JLabel("Mot de passe:");
 	private static JButton btnValiderConn = new JButton("Valider");
 	private static JButton btnAnnulerConn = new JButton("Annuler");
+	private static JLabel lblVerifier = new JLabel("");
 	/**
 	 * Create the frame.
 	 */
 	public Connexion(ControlerInterface controler) {
 		this.controler = controler;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 633, 472);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		
-		lblNewLabel.setFont(new Font("ËÎÌå", Font.PLAIN, 20));
-		lblNewLabel.setBounds(152, 0, 98, 61);
+		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 25));
+		lblNewLabel.setBounds(268, 41, 130, 74);
 		contentPane.add(lblNewLabel);
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 16));
 		
 		
-		lblNewLabel_1.setBounds(90, 77, 98, 15);
+		lblNewLabel_1.setBounds(158, 147, 112, 15);
 		contentPane.add(lblNewLabel_1);
+		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 16));
 		
 		
-		lblNewLabel_2.setBounds(90, 137, 98, 15);
+		lblNewLabel_2.setBounds(158, 219, 112, 15);
 		contentPane.add(lblNewLabel_2);
 		
 		txtIdentifiantConn = new JTextField();
-		txtIdentifiantConn.setBounds(244, 71, 115, 21);
+		txtIdentifiantConn.setBounds(374, 146, 115, 21);
 		contentPane.add(txtIdentifiantConn);
 		txtIdentifiantConn.setColumns(10);
 		
 		passConn = new JPasswordField();
-		passConn.setBounds(244, 137, 115, 21);
+		passConn.setBounds(374, 218, 115, 21);
 		contentPane.add(passConn);
 		txtIdentifiantConn.setText("");
 		passConn.setText("");
+		rdEtudiant.setFont(new Font("Arial", Font.PLAIN, 12));
 		
-		rdEtudiant.setBounds(47, 182, 121, 23);
+		rdEtudiant.setBounds(126, 284, 78, 23);
 		contentPane.add(rdEtudiant);
+		rdEnseignant.setFont(new Font("Arial", Font.PLAIN, 12));
 				
-		rdEnseignant.setBounds(170, 182, 121, 23);
+		rdEnseignant.setBounds(206, 284, 98, 23);
 		contentPane.add(rdEnseignant);
+		rdRespoF.setFont(new Font("Arial", Font.PLAIN, 12));
 				
-		rdRespoF.setBounds(293, 182, 121, 23);
+		rdRespoF.setBounds(306, 284, 138, 23);
 		contentPane.add(rdRespoF);
 		ButtonGroup g = new ButtonGroup();
 		g.add(rdEtudiant);
 		g.add(rdEnseignant);
 		g.add(rdRespoF);
+		g.add(rdAdminM);
 		
 		rdEtudiant.addItemListener(this);
 		rdEnseignant.addItemListener(this);
 		rdRespoF.addItemListener(this);
+		rdAdminM.addItemListener(this);
 				
-		btnValiderConn.setBounds(267, 232, 93, 23);
+		btnValiderConn.setBounds(399, 340, 93, 23);
 		contentPane.add(btnValiderConn);
 			
-		btnAnnulerConn.setBounds(90, 232, 93, 23);
+		btnAnnulerConn.setBounds(126, 340, 93, 23);
 		contentPane.add(btnAnnulerConn);
+		
+		
+		rdAdminM.setFont(new Font("Arial", Font.PLAIN, 12));
+		rdAdminM.setBounds(446, 284, 121, 23);
+		contentPane.add(rdAdminM);
+		
+		lblVerifier.setText("");
+		lblVerifier.setForeground(Color.RED);
+		lblVerifier.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblVerifier.setBounds(173, 394, 299, 33);
+		contentPane.add(lblVerifier);
 		
 		btnValiderConn.addActionListener(this);
 		btnAnnulerConn.addActionListener(this);
@@ -110,6 +132,8 @@ public class Connexion extends JFrame implements ItemListener, ActionListener{
 			i=2;
 		}else if(arg0.getSource()==rdRespoF) {
 			i=3;
+		}else if(arg0.getSource()==rdAdminM) {
+			i=4;
 		}
 	}
 	@Override
@@ -117,20 +141,21 @@ public class Connexion extends JFrame implements ItemListener, ActionListener{
 		if(arg0.getSource()==btnValiderConn) {
 			String id = txtIdentifiantConn.getText();
 			String mdp = new String(passConn.getPassword());
-			if(controler.connexion(id, mdp, i)!=null) {
+			Utilisateur user = controler.connexion(id, mdp, i);
+			if(user!=null) {
 			if(i==3) {
 			this.dispose();
 			HomePageAdminF hpf = new HomePageAdminF(controler);
 					}
-			else {
+			if(i==4){
 				this.dispose();
 				HomePageAdminM hp = new HomePageAdminM(controler);
 			}
 			}
 			else if(controler.connexion(id, mdp, i)==null) {
 				this.dispose();
-				System.out.println("Error");
 				Connexion conn = new Connexion(controler);
+				lblVerifier.setText("Veuillez verifier votre identifiant et mot de passe");
 			}
 
 		}
